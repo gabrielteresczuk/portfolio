0dcfa22b9fd9a2bb463097a57a45252ffc46ebb6
Gabriel Teresczuk <gabrielteresczuk@gmail.com>
2023-01-24 21:25:20 -0300

    first commit

---
+++ db.js
@@ -0,0 +1,238 @@
+let datos = [
+    {
+        id:1,
+        titulo:'EcoTurismo',
+        img:'img/ecoturismo.png',
+        fecha:'10-03-2022',
+        resumen:'Landing Page Ecoturismo',
+        descripcion:'Proyecto final para el modulo Diseño Web de Coder House. El desafio se presenta con la creacion de una landing page para mostrar lugares turisticos de la provincia de misiones usando las tecnologias de HTML+CSS+BOOSTRAP',
+        tecnologias:['HTML','CSS','JS'],
+        demo:'https://gabrielteresczuk.github.io/eco-turismo-teresczuk-final/',
+        repo:'https://github.com/gabrielteresczuk/eco-turismo-teresczuk-final',
+        dificultad:1
+    },
+    {
+        id:2,
+        titulo:'Random Quote React.js',
+        img:'img/randomQuote.png',
+        fecha:'30-03-2022',
+        resumen:'Proyecto 01 de React.js de FreeCodeCamp',
+        descripcion:'En este proyecto, se logra hacer llamadas a una API de frases, con el uso de Fetch, traer una frase al azar, montarla y modificar los colores de la aplicacion cada vez que cambie la frase.',
+        tecnologias:['HTML','CSS','JS','React js'],
+        demo:'https://gabrielteresczuk.github.io/01-random-quote/',
+        repo:'https://github.com/gabrielteresczuk/01-random-quote',
+        dificultad:3
+    },
+    {
+        id:3,
+        titulo:'MarkDown React.js',
+        img:'img/markdown.png',
+        fecha:'01-04-2022',
+        resumen:'Proyecto 02 de React.js de FreeCodeCamp',
+        descripcion:'En este proyecto se realiza un interprete de codigo de marcado, en el lado izquierdo escribimos el codigo y a la derecha podemos ver el interpretado.',
+        tecnologias:['HTML','CSS','JS','React js'],
+        demo:'https://gabrielteresczuk.github.io/02-markdown/',
+        repo:'https://github.com/gabrielteresczuk/02-markdown',
+        dificultad:3
+    },
+    {
+        id:4,
+        titulo:'Drum Machine React.js',
+        img:'img/drummachine.png',
+        fecha:'02-04-2022',
+        resumen:'Proyecto 03 de React.js de FreeCodeCamp',
+        descripcion:'En este proyecto se realiza un teclado interactivo que puede reproducir sonidos al clickear cada boton, tambien funciona presionando las teclas que corresponden al teclado qwerty.',
+        tecnologias:['HTML','CSS','JS','React js'],
+        demo:'https://gabrielteresczuk.github.io/03-drum-machine/',
+        repo:'https://github.com/gabrielteresczuk/03-drum-machine',
+        dificultad:3
+    },
+    {
+        id:5,
+        titulo:'Calculadora React.js',
+        img:'img/calculator.png',
+        fecha:'03-04-2022',
+        resumen:'Proyecto 04 de React.js de FreeCodeCamp',
+        descripcion:'En este proyecto se realiza una calculadora con memoria la cual se pueden ir anidando operaciones de suma, resta, multiplicacion y division.',
+        tecnologias:['HTML','CSS','JS','React js'],
+        demo:'https://gabrielteresczuk.github.io/04-calculator/',
+        repo:'https://github.com/gabrielteresczuk/04-calculator',
+        dificultad:3
+    },
+    {
+        id:6,
+        titulo:'Reloj Pomodoro React.js',
+        img:'img/pomodoro.png',
+        fecha:'04-04-2022',
+        resumen:'Proyecto 05 de React.js de FreeCodeCamp',
+        descripcion:'Podemos marcar los tiempo de "trabajo" y de "descanso", al dar play, comienza a descontar el tiempo hata llegar a cero, en ese punto comienza automaticamente el descanzo, luego se repite el ciclo.',
+        tecnologias:['HTML','CSS','JS','React js'],
+        demo:'https://gabrielteresczuk.github.io/05-clock/',
+        repo:'https://github.com/gabrielteresczuk/05-clock',
+        dificultad:3
+    },
+    {
+        id:7,
+        titulo:'Portafolio FCC',
+        img:'img/portfolio1.png',
+        fecha:'10-04-2022',
+        resumen:'Porfolio para FreeCodeCamp',
+        descripcion:'Creacion de un portfolio para presentar al modulo de FreeCodeCamp, el cual alberga todos los proyectos de react js',
+        tecnologias:['HTML','CSS','JS'],
+        demo:'https://gabrielteresczuk.github.io/portfolioFCC',
+        repo:'https://github.com/gabrielteresczuk/portfolioFCC',
+        dificultad:2
+    },
+    {
+        id:8,
+        titulo:'ToolBox',
+        img:'img/toolbox.png',
+        fecha:'09-05-2022',
+        resumen:'Aplicacion Ecommerce Vanilla JS',
+        descripcion:'Aplicacon realizada para aprobar el modulo JS de CoderHouse. En la misma se pueden listar y realizar compas de diferentes productos, filtrando por marca, precio, si existen ofertas o no, control de stock y al final un simple checkout.',
+        tecnologias:['HTML','CSS','JS'],
+        demo:'https://gabrielteresczuk.github.io/toolbox/',
+        repo:'https://github.com/gabrielteresczuk/toolbox',
+        dificultad:4
+    },
+    {
+        id:9,
+        titulo:'Button Maker',
+        img:'img/buttonMaker.jpg',
+        fecha:'22-05-2022',
+        resumen:'Aplicacion para crear botones',
+        descripcion:'Se realizo una aplicacion para crear estilos de CSS dinamicamente. Podes seleccionar los atributos, darle valores y el te va a devolver el codigo directo para pegar en tu boton.',
+        tecnologias:['HTML','CSS','JS','React js'],
+        demo:'https://gabrielteresczuk.github.io/buttonmaker/',
+        repo:'https://github.com/gabrielteresczuk/buttonmaker',
+        dificultad:3
+    },
+    {
+        id:10,
+        titulo:'Reaskct',
+        img:'img/reaskct.png',
+        fecha:'19-06-2022',
+        resumen:'Aplicativo de Preguntas y Respuestas',
+        descripcion:'Proyecto de fin de semana. Tenemos una base de datos en JS con preguntas, se puede variar el maximo hasta que temrine el juego, luego nos da un puntaje. Se puede responder con las letras del teclado A,B,C,D y pasar con ESPACIO.',
+        tecnologias:['HTML','CSS','JS','React js'],
+        demo:'https://gabrielteresczuk.github.io/reaskct/',
+        repo:'https://github.com/gabrielteresczuk/reaskct',
+        dificultad:4
+    },
+    {
+        id:11,
+        titulo:'MotoShop',
+        img:'img/motoshop.png',
+        fecha:'30-06-2022',
+        resumen:'Aplicativo de Ecommerce',
+        descripcion:'Proyecto final para el modulo de React.js de CoderHouse. En este proyecto abarca todo lo realacionado a react con hooks, routers y context. El proyecto se basa en una tienda de motocicletas, donde podes filtrar, agregar a favoritos, sumar al carrito, modificar stock, validar un checkout, y guardar la compra en una base de datos en firebase.',
+        tecnologias:['HTML','CSS','JS','React js','Firebase'],
+        demo:'https://gabrielteresczuk.github.io/motoshop/',
+        repo:'https://github.com/gabrielteresczuk/motoshop',
+        dificultad:5
+    },
+    {
+        id:12,
+        titulo:'Huddle landing page with curved sections solution',
+        img:'img/HuddleLanding.jpg',
+        fecha:'18-07-2022',
+        resumen:'Landing Page para FrontEnd Mentor',
+        descripcion:'Proyecto de practica para desarrollar una landing page simple a base de recursos, utilizando Grid y flexbox',
+        tecnologias:['HTML','CSS'],
+        demo:'https://gabrielteresczuk.github.io/HuddleLanding/',
+        repo:'https://github.com/gabrielteresczuk/HuddleLanding',
+        dificultad:2
+    },
+    {
+        id:13,
+        titulo:'Fylo landing page with two column layout',
+        img:'img/fylo-landing.jpg',
+        fecha:'19-07-2022',
+        resumen:'Landing Page para FrontEnd Mentor',
+        descripcion:'Proyecto de practica para desarrollar una landing page simple a base de recursos y formulario de newsletter',
+        tecnologias:['HTML','CSS'],
+        demo:'https://gabrielteresczuk.github.io/fylo-landing/',
+        repo:'https://github.com/gabrielteresczuk/fylo-landing',
+        dificultad:2
+    },
+    {
+        id:14,
+        titulo:'Huddle landing page with a single introductory section',
+        img:'img/huddle-landing-page.jpg',
+        fecha:'20-07-2022',
+        resumen:'Landing Page para FrontEnd Mentor',
+        descripcion:'Proyecto de practica super simple de una landing page con un solo grid',
+        tecnologias:['HTML','CSS'],
+        demo:'https://gabrielteresczuk.github.io/huddle-landing-page-with-single-introductory-section-master/',
+        repo:'https://github.com/gabrielteresczuk/huddle-landing-page-with-single-introductory-section-master',
+        dificultad:1
+    },
+    {
+        id:15,
+        titulo:'Fylo dark theme landing page',
+        img:'img/fylo-dark.jpg',
+        fecha:'22-07-2022',
+        resumen:'Landing Page para FrontEnd Mentor',
+        descripcion:'Proyecto de practica para desarrollar una landing page con difrentes secciones con grid, flexbox e imagenes de background',
+        tecnologias:['HTML','CSS'],
+        demo:'https://gabrielteresczuk.github.io/fylo-dark-theme-landing-page-master/',
+        repo:'https://github.com/gabrielteresczuk/fylo-dark-theme-landing-page-master',
+        dificultad:2
+    },
+    {
+        id:16,
+        titulo:'Pokedex',
+        img:'img/pokedex.png',
+        fecha:'25-07-2022',
+        resumen:'Aplicacion de practica con API Fetch',
+        descripcion:'Proyecto de creacion de una interface pokedex, utilizando los datos de la API de Pokemon, en el cual se pueden navegar dentro de los 151 primeros pokemones.',
+        tecnologias:['HTML','CSS','JS','React'],
+        demo:'https://gabrielteresczuk.github.io/pokedex/',
+        repo:'https://github.com/gabrielteresczuk/pokedex',
+        dificultad:3
+    },
+    {
+        id:17,
+        titulo:'Country API',
+        img:'img/country-API.jpg',
+        fecha:'29-07-2022',
+        resumen:'Aplicacion de practica con API Fetch y themplates',
+        descripcion:'Proyecto a modo de desafio donde hay que solicitar todos los paises y listarlos, luego poder filtrarlos con la misma API REST proporcionada por restcountries.com y poder visualizar el detalle al seleccionar algun pais de manera individual.',
+        tecnologias:['HTML','CSS','JS','React'],
+        demo:'https://gabrielteresczuk.github.io/rest-countries-api/',
+        repo:'https://github.com/gabrielteresczuk/rest-countries-api',
+        dificultad:3
+    },
+    {
+        id:18,
+        titulo:'Rock Paper Scrissor',
+        img:'img/rock-paper-scissor.jpg',
+        fecha:'03-08-2022',
+        resumen:'Juego de piedra papel y tijeras en React js',
+        descripcion:'Proyecto a modo de desafio donde hay que realizar el famoso juego de piedra, papel o tijeras, donde el usuario elije una de las 3 opciones, luego de unos segundo la computadora eligue de manera aleatoria otra opcion y se evalua quien es el ganador del juego, resaltandolo con anillos a modo de sombras. Si el resultado es positivo, sumas 1 punto, si es negativo se resta 1 punto y si es empate, no se asignan puntos.',
+        tecnologias:['HTML','CSS','JS','React'],
+        demo:'https://gabrielteresczuk.github.io/rock-paper-scissors/',
+        repo:'https://github.com/gabrielteresczuk/rock-paper-scissors',
+        dificultad:3
+    },
+    {
+        id:19,
+        titulo:'Todo App',
+        img:'img/todo-app.jpg',
+        fecha:'06-08-2022',
+        resumen:'Aplicacion de Tareas con cambio de temas',
+        descripcion:'Proyecto a modo de desafio donde hay que realizar una lista de tareas, que pueda cambiar de temas, que mantenga en memoria los datos y lo nuevo es que se puedan ordenar de manera Drag and Drop.',
+        tecnologias:['HTML','CSS','JS','React'],
+        demo:'https://gabrielteresczuk.github.io/todo-app/',
+        repo:'https://github.com/gabrielteresczuk/todo-app',
+        dificultad:4
+    }
+    
+
+
+];
+
+datos = datos.sort((a, b) => {
+    return b.dificultad - a.dificultad;
+});
+
